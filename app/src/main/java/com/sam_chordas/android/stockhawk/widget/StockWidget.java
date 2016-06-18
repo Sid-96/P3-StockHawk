6d3045aec7a8434783e1ccd0af1c6b2c6dae3476
sid <sidsinh2011@gmail.com>
2016-06-18 15:52:13 +0530

    final_project

---
+++ app/src/main/java/com/sam_chordas/android/stockhawk/widget/StockWidget.java
@@ -0,0 +1,77 @@
+package com.sam_chordas.android.stockhawk.widget;
+
+import android.annotation.TargetApi;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.widget.RemoteViews;
+
+import com.sam_chordas.android.stockhawk.R;
+import com.sam_chordas.android.stockhawk.ui.MyStocksActivity;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class StockWidget extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            // Construct the RemoteViews object
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.collection_widget);
+            // Create Intent to launch Main Activity
+            Intent intent = new Intent(context, MyStocksActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context,0,intent,0);
+            views.setOnClickPendingIntent(appWidgetId,pendingIntent);
+
+
+            Intent clickOnTemplateIntent = new Intent(context,MyStocksActivity.class);
+            PendingIntent clickOnTemplatePendingIntent = TaskStackBuilder.create(context)
+                    .addNextIntentWithParentStack(clickOnTemplateIntent)
+                    .getPendingIntent(0,PendingIntent.FLAG_UPDATE_CURRENT);
+            views.setPendingIntentTemplate(R.id.widget_list,clickOnTemplatePendingIntent);
+            // Set up collection
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+                setRemoteAdapter(context, views);
+            } else {
+                setRemoteAdapterV11(context, views);
+            }
+
+
+
+            // Instruct the widget manager to update the widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+
+    /**
+     * Sets the remote adapter used to fill in the list items
+     *
+     * @param context the context used to launch the intent
+     * @param views RemoteViews to set the RemoteAdapter
+     */
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    private void setRemoteAdapter(Context context, @NonNull final RemoteViews views) {
+        views.setRemoteAdapter(R.id.widget_list,
+                new Intent(context, QuoteWidgetRemoteViewsService.class));
+    }
+
+    /**
+     * Sets the remote adapter used to fill in the list items
+     *
+     * @param context the context to launch the intent
+     * @param views RemoteViews to set the RemoteAdapter
+     */
+    @SuppressWarnings("deprecation")
+    private void setRemoteAdapterV11(Context context, @NonNull final RemoteViews views) {
+        views.setRemoteAdapter(0, R.id.widget_list,
+                new Intent(context, QuoteWidgetRemoteViewsService.class));
+    }
+}
+
